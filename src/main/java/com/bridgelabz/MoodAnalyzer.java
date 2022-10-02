a417644102932dc0ec2c3f77f5db6cd5f2d33019
Pavithrahc <pavithramubbu93@gmail.com>
2022-10-02 08:44:29 +0530

    REFACTORED for proper packae structure

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,5 +1,5 @@
 package com.bridgelabz;
-
+import com.bridgelabz.MoodAnalysisException;
 public class MoodAnalyzer {
     private String message;
 
@@ -7,14 +7,21 @@ public class MoodAnalyzer {
         this.message = message;
     }
 
-    public String analyseMood() {
+    public String analyseMood(String message) throws MoodAnalysisException {
+        this.message = message;
+        return analyseMood();
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
         try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_EMPTY, "Please enter Proper Mood");
             if (message.contains("Sad"))
                 return "SAD";
             else
                 return "HAPPY";
         } catch (NullPointerException e) {
-            return "HAPPY";
+            throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_NULL, "Please enter Proper Mood");
         }
     }
 }
